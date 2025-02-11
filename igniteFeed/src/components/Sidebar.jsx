ebcd7a92b296f87b4730b2025c7912db175f07ea
dmardoqueu <diogomardoqueu@gmail.com>
2025-02-11 01:22:25 -0300

    Criação de Header e Sidebar

---
+++ igniteFeed/src/components/Sidebar.jsx
@@ -0,0 +1,28 @@
+import styles from './Sidebar.module.css';
+
+import { PencilLine } from 'phosphor-react';
+
+export function Sidebar() {
+    return(
+        <aside className={styles.sidebar}>
+            <img 
+            className={styles.cover}
+            src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?q=60&w=400&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            />
+
+            <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/dmardoqueu.png" />
+
+                <strong>Diôgo Mardoqueu</strong>
+                <span>Desenvolvedor de software</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    <PencilLine size={20}/>
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    )
+}
