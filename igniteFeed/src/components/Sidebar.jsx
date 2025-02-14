d2b5614a3923f5dee438eb8d31c5059591b8a030
dmardoqueu <diogomardoqueu@gmail.com>
2025-02-14 13:01:06 -0300

    Componentizando o Avatar e aplicando responsividade

--- igniteFeed/src/components/Sidebar.jsx
+++ igniteFeed/src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import styles from './Sidebar.module.css';
 
 import { PencilLine } from 'phosphor-react';
@@ -11,7 +12,7 @@ export function Sidebar() {
             />
 
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/dmardoqueu.png" />
+                <Avatar src="https://github.com/dmardoqueu.png" />
 
                 <strong>Diôgo Mardoqueu</strong>
                 <span>Desenvolvedor de software</span>
